bf6c4e0edeb9fe400b2fc719d0c50597ff5e387a
Chirag Dangodara <appso3mania3@gmail.com>
2022-04-30 11:24:47 +0530

    port change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.listen(3000, () => {
+app.listen(2828, () => {
   console.log("Application started and Listening on port 3000");
 });
 
